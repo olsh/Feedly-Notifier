2bbd8bac83837faa4b7861dd17b29459b54dc926
samodovdi <samodov.dmitry@gmail.com>
2013-07-09 16:24:52 +0400

    closed issue #3: added link to feedly home page

--- scripts/popup.js
+++ scripts/popup.js
@@ -3,18 +3,26 @@ var backgroundPage = chrome.extension.getBackgroundPage();
 function renderFeeds(){
     backgroundPage.getFeeds(function(feeds, isLoggedIn){
         if (isLoggedIn === false) {
-            $("body").children("div").hide();
             $("#login").show();
-        } else if (feeds.length === 0) {
-            $("body").children("div").hide();
-            $("#feed-empty").html("All read").show();
         } else {
-            $("body").children("div").hide();
-            $("#feed").show();
-            $('#entryTemplate').tmpl(feeds).appendTo('#feed');
-            $(".timeago").timeago();
+            $("#popup-content").show();
+
+            if (feeds.length === 0) {
+                $("#feed-empty").html("No unread articles");
+            } else {
+                $("#feed-empty").html("");
+                $('#entryTemplate').tmpl(feeds).appendTo('#feed');
+                $(".timeago").timeago();
+            }
         }
     })
+}
+
+
+function markAsRead(link){
+    if (backgroundPage.appGlobal.options.markReadOnClick === true && link.hasClass("title") === true) {
+        backgroundPage.markAsRead(link.closest(".item").data("id"));
+    }
 }
 
 renderFeeds();
@@ -23,13 +31,27 @@ $("#login").click(function () {
     backgroundPage.updateToken();
 });
 
-$("#feed").on("click", "a", function (event) {
+//using "mousedown" instead of "click" event to process middle button click.
+$("#feed").on("mousedown", "a", function (event) {
     var link = $(this);
-    chrome.tabs.create({url: link.attr("href") }, function (feedTab) {
-        if (backgroundPage.appGlobal.options.markReadOnClick === true && link.hasClass("title") === true) {
-            backgroundPage.markAsRead(link.closest(".item").data("id"));
+
+    switch(event.which){
+        //left button click - open tab
+        case 1:{
+            chrome.tabs.create({url: link.attr("href") }, function (feedTab) {
+                markAsRead(link);
+            });
+            break;
         }
-    });
+        //middle button click - just mark as read (tab opens without chrome.tabs.create on the background)
+        case 2: {
+            markAsRead(link);
+        }
+    }
+});
+
+$("#website").on("click", function (event) {
+    chrome.tabs.create({url: "http://cloud.feedly.com" }, function (feedTab) {});
 });
 
 $("#feed").on("click", ".mark-read", function (event) {
