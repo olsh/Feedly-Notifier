13a8b8fb9fb3776b5ca5723ffc6d49d4c5700992
olsh <bloodunit@gmail.com>
2013-08-07 17:50:10 +0400

    Added screenshot, sliders and FF button

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,15 @@
-console.log('This would be the main JS file.');
+$(document).on("click", ".toggle-link", function(){
+    $(this).next().slideToggle();
+});
+
+$(document).ready(function() {
+    $(".fancybox-button").fancybox({
+        prevEffect		: 'none',
+        nextEffect		: 'none',
+        closeBtn		: false,
+        helpers		: {
+            title	: { type : 'inside' },
+            buttons	: {}
+        }
+    });
+});
