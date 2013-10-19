da4713a239479bc34679badc9e98a8f81ac0a576
Oleg Shevchenko <shevchenko.oleg@outlook.com>
2013-10-20 02:35:35 +0400

    Fixed timeago bug in Czech locale

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -2,7 +2,7 @@
 
 var popupGlobal = {
     //Determines lists of supported jQuery.timeago localizations, default localization is en
-    supportedTimeAgoLocales: ["ru", "fr", "pt-BR", "it", "cz"],
+    supportedTimeAgoLocales: ["ru", "fr", "pt-BR", "it", "cs"],
     feeds: [],
     savedFeeds: [],
     backgroundPage: chrome.extension.getBackgroundPage()
