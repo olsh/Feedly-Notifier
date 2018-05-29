76100205a2800a852b5f17e3a6c00113d56409f8
clavat <cordonnier.m.76@gmail.com>
2018-05-29 13:34:35 +0200

    change condition to less than, fix sentence

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -302,7 +302,7 @@ function markAsReadEngagement() {
     var feedIds = [];
     $(".item:visible").each(function (key, value) {
         var engagement = $(value).find("span.engagement").text() || 0; //default value if no engagement: 0
-        if(engagement <= popupGlobal.backgroundPage.appGlobal.options.engagementFilterLimit) {
+        if(engagement < popupGlobal.backgroundPage.appGlobal.options.engagementFilterLimit) {
             feedIds.push($(value).data("id"));
         }
     });
